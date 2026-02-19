cc458c62cff6cd303e52667b9ded2ecc67cb595c
usipipo <usipipo@gmail.com>
2026-02-19 06:16:07 -0500

    chore: remove obsolete repositories and dependent services

--- infrastructure/persistence/postgresql/__init__.py
+++ infrastructure/persistence/postgresql/__init__.py
@@ -11,44 +11,16 @@ Version: 2.1.0
 from .base_repository import BasePostgresRepository
 from .user_repository import PostgresUserRepository
 from .key_repository import PostgresKeyRepository
-from .ticket_repository import PostgresTicketRepository
-from .transaction_repository import PostgresTransactionRepository
-from .achievement_repository import PostgresAchievementRepository, PostgresUserStatsRepository
-from .conversation_repository import PostgresConversationRepository
-from .task_repository import PostgresTaskRepository
-from .models import (
-    Base,
-    UserModel,
-    VpnKeyModel,
-    TicketModel,
-    TransactionModel,
-    AchievementModel,
-    UserStatsModel,
-    UserAchievementModel,
-    TaskModel,
-    UserTaskModel,
-    ConversationModel,
-)
+from .data_package_repository import PostgresDataPackageRepository
+from .models import Base, UserModel, VpnKeyModel, DataPackageModel
 
 __all__ = [
     "BasePostgresRepository",
     "PostgresUserRepository",
     "PostgresKeyRepository",
-    "PostgresTicketRepository",
-    "PostgresTransactionRepository",
-    "PostgresAchievementRepository",
-    "PostgresUserStatsRepository",
-    "PostgresConversationRepository",
-    "PostgresTaskRepository",
+    "PostgresDataPackageRepository",
     "Base",
     "UserModel",
     "VpnKeyModel",
-    "TicketModel",
-    "TransactionModel",
-    "AchievementModel",
-    "UserStatsModel",
-    "UserAchievementModel",
-    "TaskModel",
-    "UserTaskModel",
-    "ConversationModel",
+    "DataPackageModel",
 ]
