b3d575658f942180540becd229954709ddb1b65b
mowgliph <jelvysc@gmail.com>
2026-02-20 06:58:36 -0500

    refactor: simplify DI container removing obsolete services and handlers

--- domain/interfaces/__init__.py
+++ domain/interfaces/__init__.py
@@ -1,9 +1,12 @@
 from domain.interfaces.idata_package_repository import IDataPackageRepository
 from domain.interfaces.ikey_repository import IKeyRepository
 from domain.interfaces.iuser_repository import IUserRepository
+from domain.interfaces.itransaction_repository import ITransactionRepository, Transaction
 
 __all__ = [
     "IDataPackageRepository",
     "IKeyRepository",
     "IUserRepository",
+    "ITransactionRepository",
+    "Transaction",
 ]
